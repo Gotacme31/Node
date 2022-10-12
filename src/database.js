642d70f7dc780afd43b58e10ea35ad6592011630
Gotacme31 <abatizj2000@alumno.ipn.mx>
2022-10-12 16:25:16 -0500

    first commit

---
+++ src/database.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const { promisify } = require('util');
+const { database } = require('./keys');
+
+const pool = mysql.createPool(database);
+
+pool.getConnection((error, connection) => {
+    if(error){
+        console.log(error);
+    } 
+    if(connection) connection.release();
+    console.log("Base de datos conectada");
+    return;
+})
+
+pool.query = promisify(pool.query);
+
+module.exports = pool;
+
